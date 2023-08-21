2a09eecd569871ef9a415289bda86ed2a223e313
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-08-21 20:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ today = WEEKDAY()
 qt = Quote()
 quote_of_the_day = today.get_day()+qt.get_quote()
 cl = instagrapi.Client()
-cl.login(username=USERNAME, password=PASSWORD, verification_code="355057")
+cl.login(username=USERNAME, password=PASSWORD)
 
 notes = cl.get_notes()
 
