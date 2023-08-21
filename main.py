2eee632329a4fda6eab54b0dd1b9c6f5c4bc01dd
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-08-21 16:46:46 +0000

    add files

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ today = WEEKDAY()
 qt = Quote()
 quote_of_the_day = today.get_day()+qt.get_quote()
 cl = instagrapi.Client()
+cl.load_settings('dump.json')
 cl.login(username=USERNAME, password=PASSWORD)
 
-notes = cl.get_notes()
+
 
 cl.account_set_biography(quote_of_the_day)
-cl.logout()
+
 print(quote_of_the_day)
