4e99ba08827e15880137e5cb68380edac04a5d1a
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-08-22 13:21:35 +0530

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -13,19 +13,19 @@ class WEEKDAY:
     def get_day(self):
         emoji = random.choice(emojies)
         match (self.now.weekday()):
-            case 6:
-                return "Monday" + emoji + "\n"
             case 0:
-                return "Tuesday" + emoji + "\n"
+                return "Monday" + emoji + "\n"
             case 1:
-                return "Wednesday" + emoji + "\n"
+                return "Tuesday" + emoji + "\n"
             case 2:
-                return "Thursday" + emoji + "\n"
+                return "Wednesday" + emoji + "\n"
             case 3:
-                return "Friday" + emoji + "\n"
+                return "Thursday" + emoji + "\n"
             case 4:
-                return "Saturday" + emoji + "\n"
+                return "Friday" + emoji + "\n"
             case 5:
+                return "Saturday" + emoji + "\n"
+            case 6:
                 return "Sunday" + emoji + "\n"
             case _:
                 return ""
